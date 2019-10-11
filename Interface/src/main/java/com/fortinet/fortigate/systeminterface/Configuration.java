05af323687deea38e3fcf04dbdaf6f0a091c6bfa
Joel Cripps <jcripps@fortinet.com>
2019-10-11 12:49:17 -0700

    Adding SystemInterface handler cud
    Change-Id: I8dea8b28aa5fb674fbbc3c76e21be229a396d930

---
+++ Interface/src/main/java/com/fortinet/fortigate/systeminterface/Configuration.java
@@ -0,0 +1,17 @@
+package com.fortinet.fortigate.systeminterface;
+
+import java.io.InputStream;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+class Configuration extends BaseConfiguration {
+
+    public Configuration() {
+        super("fortinet-fortigate-systeminterface.json");
+    }
+
+    public JSONObject resourceSchemaJSONObject() {
+        return new JSONObject(new JSONTokener(this.getClass().getClassLoader().getResourceAsStream(schemaFilename)));
+    }
+
+}
